2820c1de1327811a853469c5af3ae6a3fcd20c8c
kariukijoni <kariukijoni@gmail.com>
2023-10-23 18:42:32 +0300

    Added props

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react'
+import BlogList from './BlogList'
+
+export default function Home() {
+    const [blogs,setBlogs]=useState([
+        {title:'my first blog',body:'Lorem ipsum',author:'jakes',id:1},
+        {title:'my second blog',body:'Lorem ipsum',author:'Jared',id:2},
+        {title:'my third blog',body:'Lorem ipsum',author:'jats',id:3}
+    ])
+    
+        
+  return (
+    <div className='home'>
+        
+        <BlogList blogs={blogs} title='All Blogs'/>
+    
+    </div>
+  )
+}
