0285079b2e207160109158798ef971a50cd1024f
kariukijoni <kariukijoni@gmail.com>
2023-10-24 19:54:09 +0300

    React router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import React from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router,Route,Routes,Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-      </div>
-    </div>
+    <Router>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+          <ul>
+          <li>
+              {/* <Link to='/'>Home</Link> */}
+          </li>
+          </ul>
+          <Routes>
+              <Route exact path='/' element={<Home/>}></Route>
+          </Routes>
+            
+          </div>
+        </div>
+    </Router>
   );
 }
 
