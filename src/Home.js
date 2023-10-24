dcb97de65233155d682a9f171b0824da929d3c1b
kariukijoni <kariukijoni@gmail.com>
2023-10-24 14:21:14 +0300

    condotional loading msg

--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,32 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import BlogList from './BlogList'
+import 'font-awesome/css/font-awesome.min.css';
 
 export default function Home() {
     const [blogs,setBlogs]=useState(null)
+    const [isPending,setIsPending]=useState(true)
        
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')
+             .then(res => {
+                return res.json();
+            })
+            .then(data => {
             setBlogs(data);
-        })
+            setIsPending(false)
+         })
+        },1000)
     }, []);
         
         
 
         
   return (
-    <div className='home'>
-        
-        {blogs && <BlogList blogs={blogs} title='All Blogs'/>}
+        <div className='home'>
+            {isPending && <i className="fa fa-spinner fa-spin"> </i>}
+            {blogs && <BlogList blogs={blogs} title='All Blogs'/>}
         
         {/* <BlogList blogs={blogs.filter((blog)=>blog.author==='jakes')} handleDelete={handleDelete} title="Mario's Blog"/> */}
         
