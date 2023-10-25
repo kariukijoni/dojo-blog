fd4a1c03114892f3bfb9c3757497d695fc314629
kariukijoni <kariukijoni@gmail.com>
2023-10-25 16:58:59 +0300

    Added redirects

--- src/Create.js
+++ src/Create.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
+import {useNavigate} from "react-router-dom";
+
 
 export default function Create() {
     const [title,setTitle]=useState('')
     const [body,setBody]=useState('')
     const [author,setAuthor]=useState('')
-    const[isPending,setIsPending]=useState(false)
+    const [isPending,setIsPending]=useState(false)
+    const navigate = useNavigate();
+    
     
     const handleSubmit =(e)=>{
         e.preventDefault();
@@ -23,6 +27,8 @@ export default function Create() {
         }).then(()=>{
             console.log('New blog added!')
             setIsPending(false)
+            
+            navigate('/')
         })
     }
     
@@ -38,7 +44,8 @@ export default function Create() {
             <textarea value={body} onChange={(e)=>setBody(e.target.value)} required>
             </textarea>
             <label>Blog author:</label>
-            <select value={author} onChange={(e)=>setAuthor(e.target.value)}>
+            <select value={author} onChange={(e)=>setAuthor(e.target.value)} required>
+                <option value=''>--please select--</option>
                 <option value='jakes'>jakes</option>
                 <option value='jared'>jared</option>
                 <option value='paps'>paps</option>
@@ -47,9 +54,6 @@ export default function Create() {
             { !isPending && <button>Add Blog</button> }
             { isPending && <button disabled>Adding Blog...</button> }
             
-            <p>{ title }</p>
-            <p>{ body }</p>
-            <p>{ author }</p>
         </form>
     </div>
   )
