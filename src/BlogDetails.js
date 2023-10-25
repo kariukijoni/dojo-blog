acc385f337a861ee49478183f8d95631e0d76896
kariukijoni <kariukijoni@gmail.com>
2023-10-25 17:16:40 +0300

    function delete

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
 import useFetch from './useFetch';
+import {useNavigate} from "react-router-dom";
 
 export default function BlogDetails() {
     
     const {id}=useParams();
     const{data:blog,isPending,error}=useFetch('http://localhost:8003/blogs/'+id)
+    const navigate=useNavigate()
+    
+    
+    const handleClick=()=>{
+        fetch('http://localhost:8003/blogs/'+blog.id,{
+            method:'DELETE'
+        }).then(()=>{
+            navigate('/')
+        })
+    }
     
   return (
     <div className='blog-details'>
@@ -18,6 +29,7 @@ export default function BlogDetails() {
                 <h2>{blog.title}</h2>
                 <small>Written by {blog.author}</small>
                 <div>{blog.body}</div>
+                <button onClick={handleClick}>Delete</button>
             </article>
             
         )}
